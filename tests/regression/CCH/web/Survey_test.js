c9da6f01537a305b9d624323306988331ef3805b
Luis Espejel <luis.espejel@enroutesystems.com>
2018-03-09 15:05:27 -0600

    survey test updated

--- tests/regression/CCH/web/Survey_test.js
+++ tests/regression/CCH/web/Survey_test.js
@@ -1,5 +1,13 @@
 Feature('Forms');
 
+Scenario('Login Fail', (I,loginPage) => {
+  I.amOnPage('/');
+  loginPage.loginForm('WrongUsername','WrongPass');
+  I.wait(5);
+  I.see("We didn't recognize the username or password you entered. Please try again.");
+  I.saveScreenshot('Login fail validation');
+});
+
 Scenario('Strokes Survey', (I, loginPage,surveyPage) => {
   I.amOnPage('/');
 
